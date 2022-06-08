783e3881bce434562adfb763ad4361290462b27c
olevegic <oleg.selanta@gmail.com>
2022-06-08 19:48:18 +0300

    styling things

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,7 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import { useLoader } from '@react-three/fiber'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Environment, OrbitControls } from '@react-three/drei'
 
@@ -10,12 +9,7 @@ const Scene = () => {
     const gltf = useLoader(GLTFLoader, './lieutenantHead.gltf')
     return (
         <>
-            <primitive
-                object={gltf.scene}
-                scale={3}
-                position={[0, 0, 0]}
-                rotation={3.14}
-            />
+            <primitive object={gltf.scene} scale={3} position={[0, 0, 0]} />
         </>
     )
 }
@@ -23,15 +17,12 @@ const Scene = () => {
 export const Component = () => {
     return (
         <div className="component">
-            <div>Component</div>
             <div className="container">
                 <Canvas>
                     <Suspense fallback={false}>
                         <Scene />
                         <OrbitControls />
-                        <Environment preset="dawn" background />
-                        {/* <ambientLight intensity={0.1} />
-                        <directionalLight color="white" position={[0, 0, -1]} /> */}
+                        <Environment preset="park" background />
                     </Suspense>
                 </Canvas>
             </div>
