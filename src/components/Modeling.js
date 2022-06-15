4062b578edba295e301a948563467af6e6eb9645
olevegic <oleg.selanta@gmail.com>
2022-06-15 22:48:44 +0300

    array init | start

---
+++ src/components/Modeling.js
@@ -0,0 +1,16 @@
+// adding models from array
+
+export const Modeling = arr => {
+    const arrExample = ['./dog.glb', './heli.glb']
+    const arrModels = arrExample.map(model => {
+        // model initiation to the scene
+        // loadGLTFModel(scene, '/dog.glb', {
+        //     receiveShadow: false,
+        //     castShadow: false,
+        // }).then(() => {
+        //     setLoading(false)
+        //     animate()
+        // })
+        //  +++ some styling to the boxes or containers
+    })
+}
